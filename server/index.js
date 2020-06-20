a38df584f3ffe7180c4f55a3b272b2dc692bc5ac
Ivan <ivan.morozov@list.ru>
2020-06-20 23:53:25 +0100

    hmmmm

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const port = 8000;
 
 app.use(bodyParser.json());
 
-console.log('HI');
+console.log('HI lolkek');
 app.post('/payload', (req, res) => {
     const body = req.body;
     // const branch = body.ref;
